adc4dee27315d9def9f50fd2a3180c5a0d43a840
DonatteP <pdonatte@gmail.com>
2024-04-18 22:27:02 -0300

    conectando banco de dados e adicionando paginação

---
+++ src/services/pets/getPets.ts
@@ -0,0 +1,15 @@
+import { GetPetsRequests, GetPetsResponse } from "../../interfaces/pet"
+import httpClient from "../api/httpClient"
+
+export async function getPets(
+    params?: GetPetsRequests
+): Promise<GetPetsResponse> {
+    try {
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+        const response = await httpClient.get("/pet", {params})
+        return response.data
+    } catch (error) {
+        console.log("Erro ao buscar pets", error)
+        throw error
+    }
+}   
